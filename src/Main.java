8faface16e86d18590299ce9e122b9df2eaa6e49
Rainir <off_rainir@proton.me>
2022-12-09 20:21:40 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        long minors = persons
+                .stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println("Несовершеннолетних: " + minors);
+
+        List<String> conscript = persons
+                .stream()
+                .filter(person -> person.getSex().equals(Sex.MAN))
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> person.getAge() < 27)
+                .map(Person::getFamily)
+//                .toList();  Как я понимаю везультат один и тот же с collect
+                .collect(Collectors.toList());
+//        System.out.println(conscript);
+
+        List<Person> fitness = persons
+                .stream()
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> person.getEducation().equals(Education.HIGHER))
+                .filter(person -> (person.getSex().equals(Sex.MAN) && person.getAge() < 65) || (person.getSex().equals(Sex.WOMAN) && person.getAge() < 60))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+//        System.out.println(fitness);
+    }
+}
